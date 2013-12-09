0c3d4cbc0a48ce26f20245f19e250940eb23b85b
Ian Horner <ianthorner@gmail.com>
2013-12-08 22:25:07 -0800

    Updated profile and added scoped status updates to it

--- test/integration/custom_routes_test.rb
+++ test/integration/custom_routes_test.rb
@@ -16,4 +16,10 @@ class CustomRoutesTest < ActionDispatch::IntegrationTest
 		get '/register'
 		assert_response :success
 	end
+
+	test "that a profile page works" do
+		get '/hornerian'
+		assert_response :success
+	end
+
 end
